34acce3d12df2df7dbde876d3717e3b47ccf0512
Vadim Shershen <vadim.shershen.97@mail.ru>
2023-01-17 14:35:37 +0300

    refactor: move the functions productCheck() and toggleProductToCard() out of the loop into a separate function

--- Online-store/src/controller/main-products.ts
+++ Online-store/src/controller/main-products.ts
@@ -76,40 +76,45 @@ export class MainPageController {
     const productItems: NodeListOf<HTMLDivElement> = document.querySelectorAll('.products__item');
     let productsArr: localStorageProducts[] = [];
     let cartSumNum = 0;
-    productItems.forEach((item) => {
+
+    function toggleProductToCard(item: HTMLDivElement): void {
       const itemBtn = checkSelector(item, '.products__info_btn-cart');
       const cartCount = checkSelector(document, '.cart__count');
       const sumValue = checkSelector(document, '.sum__value');
-      function toggleProductToCard() {
-        const id = item.dataset.id;
-        const count = 1;
-        const price = item.dataset.price;
-        const products = localStorage.getItem('products');
-        if (products) {
-          productsArr = JSON.parse(products) as localStorageProducts[];
-        }
-
-        if (productsArr.find((item) => item.id === id)) {
-          const index = productsArr.findIndex((el) => el.id === id);
-          productsArr.splice(index, 1);
-          cartCount.innerHTML = productsArr.length.toString();
-          itemBtn.innerHTML = 'Add to cart';
-          itemBtn.classList.remove('active');
-          cartSumNum = productsArr.reduce((sum, elem) => sum + +elem.price, 0);
-        } else {
-          let cartCountNum = productsArr.length;
-          if (id && price) productsArr.push({ id, count, price });
-          cartCountNum++;
-          cartCount.innerHTML = cartCountNum.toString();
-          itemBtn.innerHTML = 'Drop to cart';
-          itemBtn.classList.add('active');
-          cartSumNum = productsArr.reduce((sum, elem) => sum + +elem.price, 0);
-        }
-        localStorage.setItem('products', JSON.stringify(productsArr));
-        localStorage.setItem('cartSum', cartSumNum.toString());
-        sumValue.innerHTML = `€${cartSumNum}`;
+      const id = item.dataset.id;
+      const count = 1;
+      const price = item.dataset.price;
+      const products = localStorage.getItem('products');
+      if (products) {
+        productsArr = JSON.parse(products) as localStorageProducts[];
       }
-      itemBtn.addEventListener('click', toggleProductToCard);
+
+      if (productsArr.find((item) => item.id === id)) {
+        const index = productsArr.findIndex((el) => el.id === id);
+        productsArr.splice(index, 1);
+        cartCount.innerHTML = productsArr.length.toString();
+        itemBtn.innerHTML = 'Add to cart';
+        itemBtn.classList.remove('active');
+        cartSumNum = productsArr.reduce((sum, elem) => sum + +elem.price, 0);
+      } else {
+        let cartCountNum = productsArr.length;
+        if (id && price) productsArr.push({ id, count, price });
+        cartCountNum++;
+        cartCount.innerHTML = cartCountNum.toString();
+        itemBtn.innerHTML = 'Drop to cart';
+        itemBtn.classList.add('active');
+        cartSumNum = productsArr.reduce((sum, elem) => sum + +elem.price, 0);
+      }
+      localStorage.setItem('products', JSON.stringify(productsArr));
+      localStorage.setItem('cartSum', cartSumNum.toString());
+      sumValue.innerHTML = `€${cartSumNum}`;
+    }
+
+    productItems.forEach((item: HTMLDivElement) => {
+      checkSelector(item, '.products__info_btn-cart').addEventListener(
+        'click',
+        toggleProductToCard.bind(this, item)
+      );
     });
   }
 
@@ -118,6 +123,12 @@ export class MainPageController {
     const searchInput = checkSelector(document, '.search__field') as HTMLInputElement;
     const filter = searchInput.value.toLowerCase();
 
+    const productCheck = (el: Element) => {
+      if (el.textContent) {
+        return el.textContent.toLowerCase().indexOf(filter);
+      }
+    };
+
     productsItems.forEach((product: HTMLDivElement) => {
       const productDiscount = checkSelector(product, '.products__card_discount');
       const productRating = checkSelector(product, '.products__info-rating');
@@ -125,33 +136,23 @@ export class MainPageController {
       const productPriceNew = checkSelector(product, '.products__info-price_new');
       const productTitle = checkSelector(product, '.products__info-name_title');
       const productCategory = checkSelector(product, '.products__info-name_category');
-      const productRatingSpan = checkSelector(product, '.products__info-rating span');
       const productRatingCount = checkSelector(product, '.products__info-rating_count');
-      const productBrandSpan = checkSelector(product, '.products__info-brand span');
       const productBrandName = checkSelector(product, '.products__info-brand_name');
-      const productStockSpan = checkSelector(product, '.products__info-stock span');
       const productStockCount = checkSelector(product, '.products__info-stock_count');
 
-      const productCheck = (el: Element) => {
-        if (el.textContent) {
-          return el.textContent.toLowerCase().indexOf(filter);
-        }
-      };
+      const isProductSearch =
+        productCheck(productDiscount) !== -1 ||
+        productCheck(productRating) !== -1 ||
+        productCheck(productPriceOld) !== -1 ||
+        productCheck(productPriceNew) !== -1 ||
+        productCheck(productTitle) !== -1 ||
+        productCheck(productCategory) !== -1 ||
+        productCheck(productRatingCount) !== -1 ||
+        productCheck(productBrandName) !== -1 ||
+        productCheck(productStockCount) !== -1;
+
       if (product.style.display !== 'none' || !product.classList.contains('hide')) {
-        if (
-          productCheck(productDiscount) !== -1 ||
-          productCheck(productRating) !== -1 ||
-          productCheck(productPriceOld) !== -1 ||
-          productCheck(productPriceNew) !== -1 ||
-          productCheck(productTitle) !== -1 ||
-          productCheck(productCategory) !== -1 ||
-          productCheck(productRatingSpan) !== -1 ||
-          productCheck(productRatingCount) !== -1 ||
-          productCheck(productBrandSpan) !== -1 ||
-          productCheck(productBrandName) !== -1 ||
-          productCheck(productStockSpan) !== -1 ||
-          productCheck(productStockCount) !== -1
-        ) {
+        if (isProductSearch) {
           product.style.display = 'block';
         } else {
           product.style.display = 'none';
